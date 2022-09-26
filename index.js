847550ced7e07b2ddee421a0f55f051ee8100a29
Guriff <91901016+Guriff@users.noreply.github.com>
2022-09-26 20:14:42 +0300

    update

---
+++ index.js
@@ -0,0 +1,48 @@
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profilePopup = document.querySelector(".popup_type_profile");
+
+const profileFormElement = document.querySelector(".popup__form_type_profile")
+
+const nameInput = document.querySelector(".popup__input_type_name");
+const titleInput = document.querySelector(".popup__input_type_title");
+
+const profileName = document.querySelector(".profile__title_value_name");
+const profileTitle = document.querySelector(".profile__subtitle_value_title");
+
+function closePopup(popup){
+    popup.classList.remove('popup_opened')
+
+}
+
+function openPopup(popup) {
+    popup.classList.add('popup_opened')
+}
+
+profileEditButton.addEventListener('click', function(){
+
+nameInput.value = profileName.textContent;
+titleInput.value = profileTitle.textContent;
+
+openPopup(profilePopup)
+})
+
+
+
+profilePopup.addEventListener('click', function(event){
+   if (event.target.classList.contains('popup') || event.target.classList.contains('popup__close')) {
+    closePopup(profilePopup)
+   }
+})
+
+
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileTitle.textContent = titleInput.value;
+  
+    closePopup(profilePopup)
+}
+
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
