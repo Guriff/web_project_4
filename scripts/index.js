cd50d10f86356c7452dbbbaa42bf366a7a5e6412
Guriff <91901016+Guriff@users.noreply.github.com>
2022-09-27 14:25:24 +0300

    finishing touchs

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profilePopup = document.querySelector(".popup_type_profile");
 
-const profileFormElement = document.querySelector(".popup__form_type_profile")
+const profileFormElement = document.querySelector(".popup__form_type_profile");
 
 const nameInput = document.querySelector(".popup__input_type_name");
 const titleInput = document.querySelector(".popup__input_type_title");
@@ -9,40 +9,37 @@ const titleInput = document.querySelector(".popup__input_type_title");
 const profileName = document.querySelector(".profile__title_value_name");
 const profileTitle = document.querySelector(".profile__subtitle_value_title");
 
-function closePopup(popup){
-    popup.classList.remove('popup_opened')
-
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
 }
 
 function openPopup(popup) {
-    popup.classList.add('popup_opened')
+  popup.classList.add("popup_opened");
 }
 
-profileEditButton.addEventListener('click', function(){
+profileEditButton.addEventListener("click", function () {
+  nameInput.value = profileName.textContent;
+  titleInput.value = profileTitle.textContent;
 
-nameInput.value = profileName.textContent;
-titleInput.value = profileTitle.textContent;
+  openPopup(profilePopup);
+});
 
-openPopup(profilePopup)
-})
-
-
-
-profilePopup.addEventListener('click', function(event){
-   if (event.target.classList.contains('popup') || event.target.classList.contains('popup__close')) {
-    closePopup(profilePopup)
-   }
-})
-
-
+profilePopup.addEventListener("click", function (event) {
+  if (
+    event.target.classList.contains("popup") ||
+    event.target.classList.contains("popup__close")
+  ) {
+    closePopup(profilePopup);
+  }
+});
 
 function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    profileName.textContent = nameInput.value;
-    profileTitle.textContent = titleInput.value;
-  
-    closePopup(profilePopup)
+  profileName.textContent = nameInput.value;
+  profileTitle.textContent = titleInput.value;
+
+  closePopup(profilePopup);
 }
 
-profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
