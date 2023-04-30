e4e6864511b2844ebc53d245bc3450fbe09cab84
Guriff <91901016+Guriff@users.noreply.github.com>
2023-04-30 19:37:08 +0300

    fixing

--- scripts/index.js
+++ scripts/index.js
@@ -1,97 +1,45 @@
-//Warppers
-const modelWindow = document.querySelector(".popup");
-const editForm = document.querySelector(".popup__form");
-
-//Buttons and other DOM elements
 const profileEditButton = document.querySelector(".profile__edit-button");
-const modelCloseButton = document.querySelector(".popup__close");
-const profileTitle = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
+const profilePopup = document.querySelector(".popup_type_profile");
 
-//form data
-const titleInputValue = editForm.querySelector(".popup__input_type_name");
-const descriptionInputValue = editForm.querySelector(".popup__input_type_description");
+const profileFormElement = document.querySelector(".popup__form_type_profile");
 
-//card and template
+const nameInput = document.querySelector(".popup__input_type_name");
+const titleInput = document.querySelector(".popup__input_type_title");
 
-function toggleModelWindow() {
-  if (!modelWindow.cardList.contain("popup_is")) {
-    titleInputValue.value = profileTitle.textContent;
-    descriptionInputValue.value = profileDescription.textContent;
-  }
-    modelWindow.classList.toggle("popup_is-opened");
-  };
+const profileName = document.querySelector(".profile__title");
+const profileTitle = document.querySelector(".profile__subtitle");
 
-  function formSubmitHandler(evt) {
-    evt.preventDefult();
-    profileTitle.textContent = titleInputValue;
-    profileDescription.textContent = descriptionInputValue.value;
-    toggleModelWindow();
-  }
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
 
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+}
 
-  editForm.addEventListener("submit" , formSubmitHandler);
-  profileEditButton.addEventListener("click", toggleModelWindow);
-  modelCloseButton.addEventListener("click" ,toggleModelWindow);
+profileEditButton.addEventListener("click", function () {
+  nameInput.value = profileName.textContent;
+  titleInput.value = profileTitle.textContent;
 
-  const cardTemplate = document.querySelector("#card-template");
-  const cardList = document.querySelector(".cards");
-  
+  openPopup(profilePopup);
+});
 
-
-function createCard(data) {
-  const card = cardTemplate.content.querySelector(".card").cloneNode(true);
-  const imageElement = card.querySelector(".card__image");
-  const titleElement = card.querySelector(".card__title");
-
-  imageElement.src = data.url
-  titleElement.textContent = data.title;
-  return card;
-  
-};
-
-function addCardToPage(card){
-  cardList.prepend(card);
-  }
-
-  function renderCard(data) {
-    addCardToPage(createCard(data))
-
-  }
-
-  const initialCards = [
-    {
-      title: "Yosemite Valley",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
-    },
-    {
-      title: "Lake Louise",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
-    },
-    {
-      title: "Bald Mountains",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
-    },
-    {
-      title: "Latemar",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
-    },
-    {
-      title: "Vanoise National Park",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
-    },
-    {
-      title: "Lago di Braies",
-      url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-    }
-  ];
-
-  initialCards.forEach((cardData) => {
-    renderCard(cardData);
+profilePopup.addEventListener("click", function (event) {
+  if (
     
-  });
+    event.target.classList.contains("popup__close")
+  ) {
+    closePopup(profilePopup);
+  }
+});
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
 
+  profileName.textContent = nameInput.value;
+  profileTitle.textContent = titleInput.value;
 
+  closePopup(profilePopup);
+}
 
-
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
